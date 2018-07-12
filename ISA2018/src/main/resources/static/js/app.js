7d61ed94650c6bc15fcaf65a92c5e1c3a56658e7
Nikola Majstorovic <majsta93@hotmail.com>
2018-07-12 14:41:03 +0200

    Inicijalni komit:

---
+++ ISA2018/src/main/resources/static/js/app.js
@@ -0,0 +1,6 @@
+var myModule = angular.module('myModule', ['ngRoute', 'ngResource']);
+
+myModule.config(['$routeProvider', '$httpProvider','$locationProvider', function ($routeProvider, $httpProvider,$locationProvider) {
+
+}]);
+
